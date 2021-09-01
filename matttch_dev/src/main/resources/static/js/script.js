ca5bc01a0d9b752021aaea32f46aefe237d78780
Fernanda Souza <fernandalaiss@ufrn.edu.br>
2021-09-01 14:50:43 -0300

    triagem do ttt

--- matttch_dev/src/main/resources/static/js/script.js
+++ matttch_dev/src/main/resources/static/js/script.js
@@ -23,13 +23,16 @@ function goTrial(){
 }
 
 function clickTTTrial() {
+	location.href  = "trial_ttt_success.html";
+	/*
 	if (document.getElementById("img2").src == "https://brightercraft.com/wp-content/uploads/2018/10/img_3639.jpg") {
-		location.href  = "trial_ttt_success.html";
+		
 	} else {
 		document.getElementById("img1").src = "https://s3.tattoo2me.com/tattoo2me/api/media/fredao_oliveira_2_3_2019_22_12_19_38.jpg";
 		document.getElementById("img2").src = "https://brightercraft.com/wp-content/uploads/2018/10/img_3639.jpg";
 		document.getElementById("img3").src = "https://i0.wp.com/followthecolours.com.br/wp-content/uploads/2015/02/follow-the-colours-paulo-victor-skaz-tattoo-22.jpg?ssl=1";
 	}
+	*/
 }
 
 function goChat(){
@@ -42,14 +45,4 @@ function tatuador(){
 
 function verEstudio(){
     location.href = "estudio.html";
-}
-
-function clickTrial(){
-	if (document.getElementById("img2").src == "https://brightercraft.com/wp-content/uploads/2018/10/img_3639.jpg") {
-		location.href  = "trial_success.html";
-	} else {
-		document.getElementById("img1").src = "https://s3.tattoo2me.com/tattoo2me/api/media/fredao_oliveira_2_3_2019_22_12_19_38.jpg";
-		document.getElementById("img2").src = "https://brightercraft.com/wp-content/uploads/2018/10/img_3639.jpg";
-		document.getElementById("img3").src = "https://i0.wp.com/followthecolours.com.br/wp-content/uploads/2015/02/follow-the-colours-paulo-victor-skaz-tattoo-22.jpg?ssl=1";
-	}
 }
