184d0133dbf4d6f943c330dd0079213bffdb91f6
Fernanda Souza <fernandalaiss@ufrn.edu.br>
2021-09-17 09:21:20 -0300

    alterações no tatuador e perguntas

--- matttch_dev/target/classes/static/js/script.js
+++ matttch_dev/target/classes/static/js/script.js
@@ -35,6 +35,17 @@ function clickTTTrial() {
 	*/
 }
 
+function clickTrial() {
+	if (document.getElementById("img2").src == "https://brightercraft.com/wp-content/uploads/2018/10/img_3639.jpg") {
+		
+		location.href  = "trial_ttt_success.html";
+	} else {
+		document.getElementById("img1").src = "https://s3.tattoo2me.com/tattoo2me/api/media/fredao_oliveira_2_3_2019_22_12_19_38.jpg";
+		document.getElementById("img2").src = "https://brightercraft.com/wp-content/uploads/2018/10/img_3639.jpg";
+		document.getElementById("img3").src = "https://i0.wp.com/followthecolours.com.br/wp-content/uploads/2015/02/follow-the-colours-paulo-victor-skaz-tattoo-22.jpg?ssl=1";
+	}
+}
+
 function goChat(){
     location.href = "chat.html";
 }
@@ -45,4 +56,8 @@ function tatuador(){
 
 function verEstudio(){
     location.href = "estudio.html";
+}
+
+function verEstilo(){
+    location.href = "estilo.html";
 }
